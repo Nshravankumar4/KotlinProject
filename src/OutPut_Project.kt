badec11d9d970d6fc7017c25cd145741605ff0bd
Shravan Kumar Nunsavath <93977456+Nshravankumar4@users.noreply.github.com>
2025-10-23 22:15:11 +0530

    Update and rename OutPut_Project to OutPut_Project.kt

---
+++ src/OutPut_Project.kt
@@ -0,0 +1,8 @@
+// My Code is running
+// My Variable Type Function is Running
+// Val example: John
+// Var example before change: 25
+// Var example after change: 30
+
+// Process finished with exit code 0
+
