14ed27769e92f4f913adabeb83c9c0cd59e86eae
Shravan Kumar Nunsavath <93977456+Nshravankumar4@users.noreply.github.com>
2025-10-23 21:52:47 +0530

    Add files via upload (#2)
    * Add files via upload
    
    * Delete src/Main.kt

--- src/VariableType.kt
+++ src/VariableType.kt
@@ -1,21 +1,4 @@
 // Function defined separately
 fun VariableType() {
     println("My Variable Type Function is Running")
-
-    /*
-       Example for `val` (cannot be reassigned)
-    */
-    val name = "John"
-    println("Val example: $name")
-    // name = "Robert" // This would cause an error: Val cannot be reassigned
-
-    /*
-       Example for `var` (can be reassigned)
-    */
-    var age = 25
-    println("Var example before change: $age")
-
-    // Reassigning a new value
-    age = 30
-    println("Var example after change: $age")
 }
